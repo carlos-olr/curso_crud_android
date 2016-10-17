60dc8246818c6eaf27efd5d4444bedfcc840c239
Carlos Oliveira <carlos.olribeiro@gmail.com>
2016-10-17 21:49:37 -0200

    Inicial 2 - Fiz besteira

---
+++ app/src/main/java/br/com/unisal/curso/horasComplementares/activity/ActivityInical.java
@@ -0,0 +1,15 @@
+package br.com.unisal.curso.horasComplementares.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.unisal.curso.horasComplementares.R;
+
+public class ActivityInical extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inical);
+    }
+}
